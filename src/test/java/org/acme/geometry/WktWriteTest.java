f38449c6f06b8d9489bd7dde692807e26ae326c3
RoaaMasri <rouaaelmasri@gmail.com>
2022-01-04 15:05:36 +0100

    WktWriterTest

--- src/test/java/org/acme/geometry/WktWriteTest.java
+++ src/test/java/org/acme/geometry/WktWriteTest.java
@@ -3,13 +3,40 @@ package org.acme.geometry;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WktWriteTest {
     public static final double EPSILON = 1.0e-15;
 	
 	@Test
-	public void testWrite(){
+	public void testWriteEmptyPoint(){
+		Geometry g = new Point();
+        WktWrite writer = new WktWrite();
+        Assert.assertEquals("POINT EMPTY", writer.write(g));
+	}
+
+    @Test
+	public void testWriteEmptyLineString(){
+		Geometry g = new LineString();
+        WktWrite writer = new WktWrite();
+        Assert.assertEquals("LINESTRING EMPTY", writer.write(g));
+	}
+
+    @Test
+	public void testWritePoint(){
 		Geometry g = new Point(new Coordinate(3.0,4.0));
         WktWrite writer = new WktWrite();
         Assert.assertEquals("POINT(3.0 4.0)", writer.write(g));
+	}
+
+    @Test
+	public void testWriteLineString(){
+        List<Point> points = new ArrayList<Point>();
+        points.add(new Point(new Coordinate(1.0,2.0)));
+        points.add(new Point(new Coordinate(6.0,8.0)));
+		Geometry g = new LineString(points);
+        WktWrite writer = new WktWrite();
+        Assert.assertEquals("LINESTRING(1.0 2.0,6.0 8.0)", writer.write(g));
 	}
 }
